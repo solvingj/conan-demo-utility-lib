b5d15c9aa9d37f2b5da2ab349bd7e31c3a6aa5be
SSE4 <tomskside@gmail.com>
2017-09-27 10:24:17 +0700

    - arch Win32 is not correct value, pass x86

--- conaninstall.bat
+++ conaninstall.bat
@@ -1,2 +1,8 @@
-call conan install %1 --build=outdated --cwd %2 -s arch=%3 -s build_type=%4
+if "%3"=="Win32" (
+    set "ARCH=x86"
+) else (
+    set "ARCH=%3"
+)
+echo %ARCH%
+call conan install %1 --build=outdated --cwd %2 -s arch=%ARCH% -s build_type=%4
 
